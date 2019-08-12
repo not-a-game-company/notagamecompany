a1f5c845e3247bacccc263ca3289da6376aae42b
Nicolas <nicolas@quickdrawstud.io>
2019-08-12 11:59:49 -0500

    grid and zero erro fixed

--- NotAGameCompany/Assets/_Scripts/Grid.cs
+++ NotAGameCompany/Assets/_Scripts/Grid.cs
@@ -1,7 +1,10 @@
 ﻿using UnityEngine;
 public class Grid : MonoBehaviour
 {
-	[SerializeField] private float size = 1f;
+	[SerializeField] [Range(1,5)] private float size = 1f;
+	[SerializeField] private int rangeX = 40;
+	[SerializeField] private int rangeZ = 40;
+	
 	public Vector3 GetNearestPointOnGrid(Vector3 position)
 	{
 		position -= transform.position;
@@ -23,9 +26,9 @@ public class Grid : MonoBehaviour
 	private void OnDrawGizmos()
 	{
 		Gizmos.color = Color.yellow;
-		for (float x = 0; x < 40; x += size)
+		for (float x = 0; x < rangeX; x += size)
 		{
-			for (float z = 0; z < 40; z += size)
+			for (float z = 0; z < rangeZ; z += size)
 			{
 				var point = GetNearestPointOnGrid(new Vector3(x, 0f, z));
 				Gizmos.DrawSphere(point, 0.1f);
---
+++ NotAGameCompany/Assets/_Scripts/TestMove.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class TestMove : MonoBehaviour
+{ 
+	[SerializeField] private Transform destination;
+
+	[SerializeField] private int health = 5;
+	[SerializeField] private int damage = 3; 
+	
+	private NavMeshAgent navMeshagent;
+
+	public void Start()
+	{
+		navMeshagent = this.GetComponent<NavMeshAgent>();
+
+		if (navMeshagent == null)
+		{
+			Debug.LogError("Nav Mesh Agent component not found attached to " + gameObject.name);
+		}
+		else
+		{ 
+			SetDestination();
+		}
+	}
+
+	private void SetDestination()
+	{
+		if (destination != null)
+		{
+			Vector3 targetVector = destination.transform.position;
+
+			navMeshagent.SetDestination(targetVector); 
+		}
+	}
+
+	private void InAttackRange()
+	{
+		
+	}
+}
